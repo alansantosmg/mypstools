5f5adb3d919af9b7ca78e7e541e86894afaa0ace
Alan Santos <alansantosmg@gmail.com>
2024-02-15 09:42:00 -0300

    renamed file vs code shorcuts

--- snippets/general/foreach.ps1
+++ snippets/general/foreach.ps1
@@ -5,8 +5,8 @@
     get-random -minimum 1 -maximum 10
 } | 
 foreach-object {
+    
  $_ * 2
 } | 
 Measure-Object -sum -Average -Maximum -Minimum | 
 select-object -property * -ExcludeProperty Property,Count
-d
