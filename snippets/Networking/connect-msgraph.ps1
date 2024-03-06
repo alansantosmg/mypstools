7f2642e0f69303f6b63bfbf379c1351d97a4a15f
Alan Santos <alansantosmg@gmail.com>
2024-03-06 10:32:28 -0300

    Ms-Graph testes

--- snippets/Networking/connect-msgraph.ps1
+++ snippets/Networking/connect-msgraph.ps1
@@ -1,11 +1,50 @@
 # Microsoft.Graph
 
-Install-Module Microsoft.graph
+Install-Module Microsoft.graph.Users
 
 import-module Microsoft.graph.users
 
-connect-mgGraph - Scopes ‘Directory.ReadWrite.All’
+connect-mgGraph -Scopes 'User.ReadWrite.All'
 
-Get-Mguser -filter “displayName eq ‘alan’”
+$EmsSku = Get-MgSubscribedSku -All 
 
-Get-MgUser -filter “displayname eq ‘alan santos’” | fl *
+$EmsSku
+
+
+# Get-MgUser -filter "displayname eq 'alan santos'" | fl *
+
+ Get-Mguser -filter "UserPrincipalName eq 'x-gpc@dannemann-ip.com'"
+ Get-Mguser -UserId 'x-gpc@dannemann-ip.com' | Select-Object $_.licenseAssignmentStates
+
+$user = 'x-gpc@dannemann-ip.com'
+
+$user = 'alan.santos@dannemann.com.br'
+
+$userSkuPartNumber = Get-MgUserLicenseDetail -UserId $user | select-object $_.SkuPartNumber
+
+$userLicenses = Get-MgUserLicenseDetail -UserId $user |Select-Object -Property *
+$userLicenses
+
+$userSkuPartNumber.licenseAssignmentStates
+
+$user.licenseAssignmentStates
+
+# Set-MgUserLicense -UserId $user -AddLicenses @() -RemoveLicenses @($EmsSku.SkuId)
+
+
+# help Get-MgUserLicenseDetail  -Detailed
+
+Disconnect-Graph
+
+
+
+$allLicenses = Get-MgUserLicenseDetail -UserId $user -Property SkuPartNumber, ServicePlans
+$allLicenses | ForEach-Object {
+    Write-Host "License:" $_.SkuPartNumber
+    $_.ServicePlans | ForEach-Object {$_}
+}
+
+
+
+
+Get-MsolGroup -All | Where {$_.Licenses}
